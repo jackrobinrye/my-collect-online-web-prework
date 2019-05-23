9426012ef7c9aeedec38dd85317bcbd43b82bb3a
Jack Atherton <jackrrye@gmail.com>
2019-05-23 23:25:40 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   i = 0 
   collecttion2 = []
   while i < collection.size do
-    collection2 = yield(collection[i])
+    collection2 << yield(collection[i])
     i+=1 
   end 
   collection2 
